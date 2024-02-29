3fe0646a536a1a57bcbfde32d81df4a42efba583
Eric boadu <ericboadu888@gmail.com>
2024-02-29 18:47:30 -0500

    second commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Address Book Project
 # 2/28/2024
 
+import json
 import tkinter as tk
 from tkinter import messagebox
 
@@ -12,6 +13,7 @@ class AddressBookGUI:
         self.master = master
         self.master.title("Address Book")
 
+        self.address_book = AddressBook()
         self.master.geometry("400x100")
 
         self.name_text = tk.Label(master, text="Name:")
@@ -37,5 +39,48 @@ class AddressBookGUI:
 
         self.add_btn = tk.Button(master, text="List all Current Contact", command=self.List_all) 
         self.add_btn.grid(row=3, column=2, padx=2, pady=2)
+
+
+    def add_contact(self):
+        name = self.name_textBox.get()
+        phone = self.phone_textBox.get()
+        email = self.email_textBox.get()
+
+        if name and phone and email:
+            added_successfully = self.address_book(name, phone, email)
+
+            if added_successfully: 
+                messagebox.showinfo("success", "Contact has been added ^_^ !")
+                self.save_to_json()
+            else: 
+                messagebox.showerror("error", "Detected duplicate contact ~_~ .")
+        else: 
+            messagebox.showerror("Error", "Please fill in all field needed U_U .") 
+
+
+    def save_to_json(self): 
+        with open("address_book.json", "w") as json_file:
+            json.dump(self.address_book, json_file)
+    
+    
+
+class AddressBook:
+    def __init__ (self):
+        self.contact = []
+    
+   
+def main():
+    root = tk.Tk()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
+
+
+       
+    
+
+
 
         
