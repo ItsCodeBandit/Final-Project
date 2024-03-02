b25212fc93f71cf06dc42ebb66625e48ab2df032
Eric boadu <ericboadu888@gmail.com>
2024-03-02 16:57:20 -0500

    Your second commit message here

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from tkinter import messagebox
 
 
 class AddressBookGUI:
-    def __int__(self, master):
+    def __init__(self, master):
         self.master = master
         self.master.title("Address Book")
 
@@ -18,27 +18,27 @@ class AddressBookGUI:
 
         self.name_text = tk.Label(master, text="Name:")
         self.name_text.grid(row=0, column=0)
-        self.name_textBox = tk.Entry(master, width=50)
+        self.name_textBox = tk.Entry(master, width=30)
         self.name_textBox.grid(row=0, column=1)
 
         self.phone_text = tk.Label(master, text="phone:")
         self.phone_text.grid(row=1, column=0)
-        self.phone_textBox = tk.Entry(master, width=50)
+        self.phone_textBox = tk.Entry(master, width=30)
         self.phone_textBox.grid(row=1, column=1)
 
         self.email_text = tk.Label(master, text="Email:")
         self.email_text.grid(row=2, column=0)
-        self.email_textBox = tk.Entry(master, width=50)
+        self.email_textBox = tk.Entry(master, width=30)
         self.email_textBox.grid(row=2, column=1)
 
         self.add_btn = tk.Button(master, text="Add Contact", command=self.add_contact) 
         self.add_btn.grid(row=3, column=0, padx=2, pady=2)
 
-        self.add_btn = tk.Button(master, text="Search Contact", command=self.Search_Content) 
-        self.add_btn.grid(row=3, column=1, padx=2, pady=2)
+        self.search_btn = tk.Button(master, text="Search Contact", command=self.Search_Content) 
+        self.search_btn.grid(row=3, column=1, padx=2, pady=2)
 
-        self.add_btn = tk.Button(master, text="List all Current Contact", command=self.List_all) 
-        self.add_btn.grid(row=3, column=2, padx=2, pady=2)
+        self.list_btn = tk.Button(master, text="List all Current Contact", command=self.list_all) 
+        self.list_btn.grid(row=3, column=2, padx=2, pady=2)
 
 
 
@@ -62,10 +62,10 @@ class AddressBookGUI:
         with open ("address_book.json", "w") as json_file: 
             json.dump(self.address_book.contacts, json_file)
 
-    def search_contact(self): 
+    def Search_Content(self): 
         name = self.name_textBox.get()
         if name: 
-            contact = self.address_book.search_contact(name)
+            contact = self.address_book.Search_Content(name)
             if contact:
                 messagebox.showinfo("Contact Details", f"Name: {name}\nPhone: {contact['phone']}\nEmail: {contact['email']}")
             else: 
@@ -84,11 +84,30 @@ class AddressBookGUI:
 
 class AddressBook:
     def __init__ (self):
-        self.contact = []
+        self.contacts = []
     
-   
+    def add_contact(self, name, phone_num, email): 
+        for contact in self.contacts: 
+            if contact['name'] == name: 
+
+                return False
+            
+        contact = {'name': name, 'phone': phone_num, 'email': email}
+        self.contacts.append(contact)
+        return True
+    
+    def Search_Content(self, name): 
+        for contact in self.contacts: 
+            if contact['name'] == name: 
+                return contact 
+        return None 
+        
+    def list_all(self): 
+        return self.contacts 
+    
 def main():
     root = tk.Tk()
+    app = AddressBookGUI(root)
     root.mainloop()
 
 
